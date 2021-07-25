bcaf6d76bcf6585706bd93b3f204f5fd2ad9a76e
Sebastián Robles Cagliero <sebaroblesca@gmail.com>
2021-07-25 17:29:04 -0400

    make it better

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ app.use(cors());
 app.use('/torneos', torneoRoutes);
 app.use('/user', userRouter);
 
-const CONNECTION_URL = "mongodb+srv://sebazungui:1633186@cluster0.cqn1x.mongodb.net/db-name?retryWrites=true&w=majority";
-const PORT = process.env.PORT|| 5000;
+app.get('/', (req, res) => {
+  res.send('Bienvenido a ligacommander api');
+});
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
